0de010c0f2d41973c7d67fb271d1922abbd2fc6c
Adi Eswar <aditya.eswar@icloud.com>
2020-04-19 14:34:39 -0400

    New assignment #2 coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a list containing a function to set and get value of the matrix plus set and get the value of its invers. 
+## repurposing the given mean template 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver <- NULL
+  set <- function(y) {
+    x <<- y
+    inver <<- NULL
+  }
+  get <- function() x
+  setinvererse <- function(invererse) inver <<- invererse
+  getinvererse <- function() inver
+  list(set=set, get=get, setinvererse=setinvererse, getinvererse=getinvererse)
 }
 
 
-## Write a short comment describing this function
+## Function to return the inver of a matrix. If the inver is stored it will retreive it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inver <- x$getinvererse()
+  if(!is.null(inver)) {
+    message("getting cached data.")
+    return(inver)
+  }
+  data <- x$get()
+  inver <- solve(data)
+  x$setinvererse(inver)
+  inver
 }
